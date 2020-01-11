f56116c8387d08204319b0e75baa997997a606db
Persephone007 <persephone007@gmail.com>
2020-01-10 20:14:32 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
-
-
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
 
-
-
-
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-
-
-
-
-
+mongo = PyMongo(app)
 @app.route("/")
 def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars)
 
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_scrape = scrape_mars.news()
+    mars_scrape = scrape_mars.nasa_image()
+    mars_scrape = scrape_mars.weather()
+    mars_scrape = scrape_mars.mars_facts()
+    mars_scrape = scrape_mars.hemispheres()
+    
+    mars.update({}, mars_scrape, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,17 +5,16 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-    def init_browser():
-    executable_path = {"executable_path": 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+def init_browser():
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
     
-    def scrape_info():
+#def scrape_info():
+    #browser = init_browser()
+mars_scrape = {}
+    
+def news():
     browser = init_browser()
-
-    mars_scrape = {}
-    
-    
-    """""" news
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
     html = browser.html
@@ -27,8 +26,12 @@ import time
     news_p = results.find("div", class_ ="article_teaser_body").text
     mars_scrape["news_title"] = news_title
     mars_scrape["news_p"] = news_p
+    return mars_scrape
+
+    browser.quit()
     
-    """""" nasa image
+def nasa_image():
+    browser = init_browser()
     mars = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(mars)
     html = browser.html
@@ -37,23 +40,34 @@ import time
     img = find_img.get('src')
     featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA23511-640x350.jpg'
     mars_scrape["featured_image_url"] = featured_image_url
+    return mars_scrape
 
-    """""" weather
+    browser.quit()
+
+def weather():
+    browser = init_browser()
     twitter = "https://twitter.com/MarsWxReport/status/1196244436252078080"
     browser.visit(twitter)
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
     mars_weather = soup.find('div', class_='js-tweet-text-container').text
     mars_scrape["mars_weather"] = mars_weather
-    
-    """""" mars facts
+    return mars_scrape
+
+    browser.quit()
+
+def mars_facts():
+    browser = init_browser()
     mars_facts = "https://space-facts.com/mars/"
     mars_tables = pd.read_html(mars_facts)
     facts_df = pd.DataFrame(mars_tables[0])
     mars_scrape["facts_df"] = facts_df
+    return mars_scrape
 
-    """""" henispheres
-    
+    broswer.quit()
+
+def hemispheres():
+    browser = init_browser()
     hemisphere_image_urls = [
     {"title": "Valles Marineris Hemisphere", "img_url":      "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif"},
     {"title": "Cerberus Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif"},
@@ -64,3 +78,5 @@ import time
     mars_scrape["hemisphere_image_urls"] = hemisphere_image_urls
     
     return mars_scrape
+
+    browser.quit()
